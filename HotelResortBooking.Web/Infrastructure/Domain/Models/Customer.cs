cb3b519a68db3edaeb34f4ee519b51a8aa58910c
Wesley S. A <jwsalipio@csmbataan.ph.education>
2021-03-04 22:08:11 +0800

    Customer Class

---
+++ HotelResortBooking.Web/Infrastructure/Domain/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelResortBooking.Web.Infrastructure.Domain.Models
+{
+    public class Customer : BaseModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string HomeAddress { get; set; }
+
+        public string ContactNumber { get; set; }
+    }
+}
